5199149b59c538cac6d1073f7f3a85257f462bf4
Hussian Alamri <husain.amri@me.com>
2018-02-24 19:05:54 -0500

    Added RGB support

--- SwatchEncoder.hpp
+++ SwatchEncoder.hpp
@@ -15,48 +15,51 @@
 namespace SwatchEncoder {
 
     static const uint16_t kColorSpaceRGB        = 0;
-    static const uint16_t kColorSpaceHSB        = 1;
-    static const uint16_t kColorSpaceCMYK       = 2;
+    static const uint16_t kColorSpaceHSB        = 1;    // Not currently implemented
+    static const uint16_t kColorSpaceCMYK       = 2;    // Not currently implemented
     static const uint16_t kColorSpaceLAB        = 7;
-    static const uint16_t kColorSpaceGrayscale  = 8;
+    static const uint16_t kColorSpaceGrayscale  = 8;    // Not currently implemented
 
     /**
-     * @typedef     aco_struct_v1
+     * @typedef     aco_struct_lab
      * @brief       This struct holds the information for each color
-     * @constant    L       Lab value L
-     * @constant    a       Lab value a
-     * @constant    b       Lab value b
-     */
-    typedef struct aco_struct_v1 {
-        float L;
-        float a;
-        float b;
-    } aco_struct_v1;
-
-    /**
-     * @typedef     aco_struct_v2
-     * @brief       This struct holds the information for each color
-     * @constant    L       Lab value L
-     * @constant    a       Lab value a
-     * @constant    b       Lab value b
+     * @constant    L       Lab value L         ( range: 0..10000 )
+     * @constant    a       Lab value a         ( range: -12800..12700 )
+     * @constant    b       Lab value b         ( range: -12800..12700 )
      * @constant    len     Length of name (number of characters in name) not including null value if null-terminated string
      * @constant    name    Name of color (no null terminator), must be in UTF-16 Big Endian
      */
-    typedef struct __aco_struct_v2 {
-        float L;
-        float a;
-        float b;
-        size_t len;
-        uint16_t* name;
-    } aco_struct_v2;
-
+    typedef struct __aco_struct_lab {
+        int16_t L;
+        int16_t a;
+        int16_t b;
+        size_t len;         // [Optional] pass 0 if you don't want to assign a name to the color
+        uint16_t* name;     // [Optional] name will be ignored if len is 0
+    } aco_struct_lab;
+    
+    /**
+     * @typedef     aco_struct_rgb
+     * @brief       This struct holds the information for each color
+     * @constant    R       RGB value Red       ( range: 0..65535 )
+     * @constant    G       RGB value Green     ( range: 0..65535 )
+     * @constant    B       RGB value Blue      ( range: 0..65535 )
+     * @constant    len     Length of name (number of characters in name) not including null value if null-terminated string
+     * @constant    name    Name of color (no null terminator), must be in UTF-16 Big Endian
+     */
+    typedef struct __aco_struct_rgb {
+        uint16_t R;
+        uint16_t G;
+        uint16_t B;
+        size_t len;         // [Optional] pass 0 if you don't want to assign a name to the color
+        uint16_t* name;     // [Optional] name will be ignored if len is 0
+    } aco_struct_rgb;
     /**
      * @brief   encodes an array of ACO structs into a .aco file, no error reporting
      * @param   nColors     size_t
      * @param   colors      aco_struct_v1
      * @return              vector<uint8_t>     binary data that can be written to file
      */
-    std::vector<uint8_t> encodeSwatch(aco_struct_v1* colors, size_t nColors);
+    std::vector<uint8_t> encodeSwatch_lab(aco_struct_lab* colors, size_t nColors);
 
     /**
      * @brief   encodes an array of ACO structs into a .aco file, no error reporting
@@ -64,6 +67,6 @@ namespace SwatchEncoder {
      * @param   colors      aco_struct_v2
      * @return              vector<uint8_t>     binary data that can be written to file
      */
-    std::vector<uint8_t> encodeSwatch(aco_struct_v2* colors, size_t nColors);
+    std::vector<uint8_t> encodeSwatch_rgb(aco_struct_rgb* colors, size_t nColors);
 }
 
